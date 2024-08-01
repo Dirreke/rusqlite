f78115d516da24fd5cc41aa24ca59281919df3f3
Dirreck <mingyang_ge@163.com>
2024-08-01 18:03:18 +0800

    Update build.rs

--- libsqlite3-sys/build.rs
+++ libsqlite3-sys/build.rs
@@ -223,6 +223,30 @@ mod build_bundled {
         // the bundled sqlite on android also uses `SQLITE_TEMP_STORE=3`.
         // https://android.googlesource.com/platform/external/sqlite/+/2c8c9ae3b7e6f340a19a0001c2a889a211c9d8b2/dist/Android.mk
         if super::android_target() {
+                let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH not set");
+                if target_arch == "x86_64" {
+                    let android_ndk_home = env::var("ANDROID_NDK_HOME").expect("ANDROID_NDK_HOME not set");
+                    let build_os = match env::consts::OS {
+                        "linux" => "linux",
+                        "macos" => "darwin",
+                        "windows" => "windows",
+                        _ => panic!(
+                            "Unsupported OS. You must use either Linux, MacOS or Windows to build the crate."
+                        ),
+                    };
+                    let clang_version =
+                        env::var("NDK_CLANG_VERSION").unwrap_or_else(|_| DEFAULT_CLANG_VERSION.to_owned());
+                    let linux_x86_64_lib_dir = format!(
+                        "toolchains/llvm/prebuilt/{build_os}-x86_64/lib/clang/{clang_version}/lib/linux/"
+                    );
+                    let linkpath = format!("{android_ndk_home}/{linux_x86_64_lib_dir}");
+                    if Path::new(&linkpath).exists() {
+                        println!("cargo:rustc-link-search={android_ndk_home}/{linux_x86_64_lib_dir}");
+                        println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");
+                    } else {
+                        panic!("Path {linkpath} not exists");
+                    }
+                }
             cfg.flag("-DSQLITE_TEMP_STORE=3");
         }
 
